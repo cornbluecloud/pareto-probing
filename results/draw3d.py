e2db9f0bc18585a531d5b358585e00a8aefa3283
My Phan <myphan@cs.umass.edu>
2020-12-03 11:06:20 -0500

    reduced training size for BERT_d80 and BERT_d20

--- results/draw3d.py
+++ results/draw3d.py
@@ -13,11 +13,10 @@ prob_acc = 5
 #for prob_complexity in range(40, 310, 10):
 for r in [ "power3"]: #[0.01, 0.1, 1, 10 ]:
     for d in [0]: #range(0, 100, 10):
-        x, y, z  = [], [], []
-        s = 100 #range(10, 110, 10):
-        if True:
-    #   filename = "../reduced_training_data/d0_" + str(p)+"_bert/all_results.tsv"
-            filename = "../inverse_power/B_" + r +"_d"+ str(d)+"_100_bert/all_results.tsv"
+
+        for s in range(10, 110, 10): #s = 100 #range(10, 110, 10):
+            x, y, z  = [], [], []
+            filename = "../B_bert/B_power3_d0_" +str(s) +"_bert/all_results.tsv"
             reader = csv.reader(open(filename), delimiter = ",")
             representation = 'bert' # 22
             model = 'linear'
@@ -32,103 +31,24 @@ for r in [ "power3"]: #[0.01, 0.1, 1, 10 ]:
                 break
 
             # save the test data in selected rows
-            z0= []
+
             for row in reader:
                 opt = True
          #      opt = (row[ref['representation']] == representation and row[ref['model']] == model and row[ref['language']] == language)
                 if opt:
-                    if float(row[ref[x_axis]]) > 500:   # drop the outliers
-                        continue
+                    # if float(row[ref[x_axis]]) > 500:   # drop the outliers
+                    #     continue
                     # if np.abs(float(row[ref[x_axis]]) - prob_complexity) > prob_acc:   # drop the outliers
                     #     continue
                     x.append(float(row[ref[x_axis]]))
                     y.append(s)
                     z.append(float(row[ref[y_axis]]))
-        # print(x)
-        # print(z)
-        if d == 80:
-            plt.scatter(x,z, label = "R"+ str(r)+"_"+ str(d) , marker = "*")
-        else:
-            plt.scatter(x,z, label = "R"+ str(r)+"_"+ str(d))
+
+            plt.scatter(x,z, label = "Rpower3_"+ "training_size_"+str(s) )
 
 
     #    ax.plot(x, y, z, label = "R1_"+ str(d) + "% d")
     #ax = plt.axes(projection='3d')
-if True:
-    for d in [00]: #range(0, 100, 10):
-
-        for r in [0]: #ange(3, 11):
-            x, y, z  = [], [], []
-            filename = "../multiple_bijections/R0" + str(r) +"_d0_" + str(s)+"_bert/all_results.tsv"
-
-            reader = csv.reader(open(filename), delimiter = ",")
-            representation = 'bert' # 22
-            model = 'linear'
-            language = 'english'
-            x_axis = 'test_norm'
-            y_axis = 'test_acc'
-
-
-            for row in reader:
-                order = len(row)
-                ref = {row[i]: i for i in range(order)}
-                break
-
-            # save the test data in selected rows
-            z0= []
-            for row in reader:
-                opt = True
-         #      opt = (row[ref['representation']] == representation and row[ref['model']] == model and row[ref['language']] == language)
-                if opt:
-                    if float(row[ref[x_axis]]) > 500:   # drop the outliers
-                        continue
-                    # if np.abs(float(row[ref[x_axis]]) - prob_complexity) > prob_acc:   # drop the outliers
-                    #     continue
-                    x.append(float(row[ref[x_axis]]))
-                    y.append(s)
-                    z.append(float(row[ref[y_axis]]))
-
-            if d==0:
-                plt.scatter(x,z, label = "R0"+ str(r)+"_"+str(d))
-            # # else:
-            #     plt.scatter(x,z, label = "R0"+ str(r)+"_"+str(d), marker = "*")
-  ##      ax.plot(x, y, z, label = "R0_"+ str(d) + "% d")
-if True:
-    for d in range(80, 100, 10):
-
-        if True:
-            x, y, z  = [], [], []
-            filename = "../missing_data_pos_tag/d" +str(d)+"_bert/all_results.tsv"
-
-            reader = csv.reader(open(filename), delimiter = ",")
-            representation = 'bert' # 22
-            model = 'linear'
-            language = 'english'
-            x_axis = 'test_norm'
-            y_axis = 'test_acc'
-
-
-            for row in reader:
-                order = len(row)
-                ref = {row[i]: i for i in range(order)}
-                break
-
-            # save the test data in selected rows
-            z0= []
-            for row in reader:
-                opt = True
-         #      opt = (row[ref['representation']] == representation and row[ref['model']] == model and row[ref['language']] == language)
-                if opt:
-                    if float(row[ref[x_axis]]) > 500:   # drop the outliers
-                        continue
-                    # if np.abs(float(row[ref[x_axis]]) - prob_complexity) > prob_acc:   # drop the outliers
-                    #     continue
-                    x.append(float(row[ref[x_axis]]))
-                    y.append(s)
-                    z.append(float(row[ref[y_axis]]))
-
-            plt.scatter(x,z, label = "R0_"+str(d), marker = "*")
-    # print(x)
 
 
     # ax.set_title('Pareto Curves')
